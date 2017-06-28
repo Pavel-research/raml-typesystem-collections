0ccad3241233a4637ae7a8b5592d61fe07d70657
Pavel Petrochenko <petrochenko.pavel.a@gmail.com>
2017-06-28 22:47:32 +0600

    on the way.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import n=require("callables-rpc-views")
 import model=require("raml1-domain-model")
+import col=require("./collection");
 
-
-export interface BasicCollection{
-
-    range(): model.Type
-
-    forEach(f: (v: any, i?: number) => void): Promise<any>
-
-    map(f: (v: any, i?: number) => any): Promise<any[]>
-}
-
+export import BasicCollection=col.BasicCollection;
 
 export interface Collection extends BasicCollection{
 
@@ -32,191 +24,103 @@ export interface Collection extends BasicCollection{
 
 }
 
-export interface PagingSpec{
-
-    total?: string
-    result?: string
+export interface PagingSpecification{
     offset?: string
     page?: string
     limit?: string
-    isPagedByLinkHeader?:boolean
+}
+export interface FilterSpecification{
+
+    property?: string
+    valueMappings:{ [name:string]:any}
+
+    noFilterValue: string
+
+    filterOp: "eq"|"lt"|"gt"|"like"|"ge"|"le"
+}
+export interface OrderingMapping{
+    property:string
+    descending?: boolean
 }
 
-export class BasicPagedCollection implements Collection {
+export interface OrderingMappings{
 
-    parameters(): n.Parameter[] {
-        return this._parameters;
-    }
+    [name:string]:OrderingMapping| string
 
-    private _pValues = {};
-    private _parameters:model.Parameter[];
-    private _currentPage:any[];
-
-    private hasTotal: boolean;
-    private _total: number;
-
-    private _elementsPerPage: number
-
-    private _pagingSpec:PagingSpec;
-
-    pagingSpec():PagingSpec{
-        if (this._pagingSpec){
-            return this._pagingSpec;
-        }
-        this._pagingSpec={};
-        var ps:PagingSpec=this._n.annotation("paging");
-        if(ps){
-            this._pagingSpec=ps;
-        }
-        if (!this._pagingSpec.offset&&!this._pagingSpec.page){
-            this._pagingSpec.isPagedByLinkHeader=true;
-        }
-        if (this._pagingSpec.result){
-            var prop=this._n.returnType().property(this._pagingSpec.result);
-            if (prop) {
-                this._range = prop.range().componentType();
-            }
-            else{
-                 throw new Error("Can not resolve collection type");
-            }
-        }
-        return this._pagingSpec;
-    }
-    private _offset:number=0;
-
-    buildCallParameters(){
-        var pVals={};
-        Object.keys(this._pValues).forEach(x=>{
-            pVals[x]=this._pValues[x];
-        })
-        let pagingSpec = this.pagingSpec();
-        if (pagingSpec.offset){
-            pVals[pagingSpec.offset]=this._offset;
-        }
-        if (pagingSpec.page){
-            pVals[pagingSpec.page]=this._offset;
-        }
-        return pVals;
-    }
-
-    setParameterValue(n: string, value: any) {
-        this._pValues[n] = value;
-    }
-    _range: model.Type;
-
-    constructor(private _n: n.CallableFunction) {
-        var mm=this._n.annotation("collection");
-        if (mm){
-           this._range= this._n.returnType().registry().getType(<any>mm);
-        }
-        else{
-            this._range=this._n.returnType().componentType();
-        }
-        var ps=this.pagingSpec();
-        let filteredPars:model.Parameter[]=[];
-        this._n.parameters().forEach(x => {
-            let s = x.name();
-            if(s==ps.offset||s==ps.page||s==ps.limit){
-                return;
-            }
-            filteredPars.push(x);
-        });
-        this._parameters=filteredPars;;
-    }
-
-    page(): Promise<any[]> {
-        if (this._currentPage){
-            return Promise.resolve(this._currentPage);
-        }
-        return this._n.call(this.buildCallParameters()).then(x=>{
-            let pagingSpec = this.pagingSpec();
-            if (pagingSpec.total){
-                this.hasTotal=true;
-                this._total=x[pagingSpec.total];;
-            }
-            if (pagingSpec.result){
-                this.hasTotal=true;
-                this._currentPage=x[pagingSpec.result];
-            }
-            else{
-                this._currentPage=x;
-            }
-            if (this.hasTotal){
-                if (this._total>this._currentPage.length){
-                    this._elementsPerPage=this._currentPage.length;
-                }
-            }
-            return this._currentPage;
-        })
-    }
-
-    nextPage(): Promise<any[]> {
-        this._currentPage=null;
-        this._offset++;
-        return this.page();
-    }
-
-    hasNextPage() {
-
-    }
-
-    hasPrevPage() {
-        if (this._offset>0){
-            return true;
-        }
-    }
-
-    previousPage(): Promise<any[]> {
-        if (this._offset>0) {
-            this._currentPage = null;
-            this._offset--;
-        }
-        return this.page();
-    }
-
-    firstPage(): Promise<any[]> {
-        if(this._offset!=0) {
-            this._offset == 0;
-            this._currentPage = null;
-        }
-        return this.page();
-    }
-
-    lastPage(): Promise<any[]> {
-        if (this.hasTotal&&this._elementsPerPage){
-
-        }
-        return null;
-    }
-
-    total(): Promise<any> {
-        if (this.hasTotal===false||this.hasTotal===true){
-            return Promise.resolve(this._total);
-        }
-        return this.page().then(x=>{return this._total});
-    }
-
-    range(): model.Type {
-        return this._range;
-    }
-
-    forEach(f: (v: any, i?: number) => void) {
-        this.firstPage().then(x=>{
-            if (this._elementsPerPage){
-
-            }
-        })
-        return null;
-    }
-
-    map(f: (v: any, i?: number) => any) {
-        var mm=[];
-        return this.forEach((x,i)=>{
-            mm.push(f(x,i));
-        }).then(x=>Promise.resolve(mm));
-    }
+}
+export enum CollectionKind{
+    OFFSETBASED, PAGEBASED, LINKBASED
 }
 
+export interface CollectionInfo{
+    paging?:PagingSpecification
+
+    total?: string
+
+    result?: string
+
+    filters?: {
+        [name:string]:FilterSpecification
+    }
+
+    ordering?: string
+
+    orderingMappings?: OrderingMappings
+
+    sortDirection?: string
+
+    kind:CollectionKind
+
+    range: model.Type
+}
+
+export function buildCollectionInfo(n:n.CallableFunction):CollectionInfo{
+    let range:n.Type=null;
+    let kind: CollectionKind.LINKBASED;
+
+    var pspec:PagingSpecification={};
+    if (n.returnType().isArray()){
+        range=n.returnType().componentType();
+    }
+    else{
+        n.returnType().properties().forEach(x=>{
+            let range = x.range();
+            if (hasAnnotation(range,"result")){
+                range=range;
+            }
+            if (hasAnnotation(range,"offset")){
+                pspec.offset=x.name();
+            }
+            if (hasAnnotation(range,"page")){
+                pspec.page=x.name();
+            }
+            if (hasAnnotation(range,"limit")){
+                pspec.limit=x.name();
+            }
+        })
+        var ps=n.annotation("paging");
+        if (ps){
+            var res=ps.result;
+            if (res){
+                range=n.returnType().property(res).range();
+            }
+            pspec.offset=ps.offset;
+            pspec.page=ps.page;
+            pspec.limit=ps.limit;
+        }
+    }
+    if (!range){
+        return null;
+    }
+    let collectionInfo:CollectionInfo={ range:range, kind:CollectionKind.OFFSETBASED};
+    return collectionInfo;
+}
+
+function hasAnnotation(t:model.Type,n:string){
+    var has=false;
+    t.annotations().forEach(x=>{if (x.name()==n||x.name().endsWith("."+n)){has=true}})
+    return has;
+}
 export function isCollection(n: n.CallableFunction): boolean {
     var rs=false;
     if (n.returnType().isArray()&&n.isSafe()){
@@ -225,7 +129,9 @@ export function isCollection(n: n.CallableFunction): boolean {
     if (n.hasAnnotation("paging")){
         return true;
     }
+    n.parameters().forEach(x=>{
 
+    })
     return rs;
 }
 
